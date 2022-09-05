d6abc6e43725bbf175841ef4bea1b3d02a73d172
hoxtonr <3509362670@qq.com>
2022-09-04 22:19:19 -0400

    test2

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use std::io::{stdout, Read, Write,stdin};
 use std::net::TcpStream;
 use std::time::{SystemTime, UNIX_EPOCH};
 use rustls::{OwnedTrustAnchor, RootCertStore};
-fn main() {
-    
+use std::thread;
+use std::sync::mpsc;
+
+fn root_creator() -> rustls::ClientConfig{
     let mut root_store = RootCertStore::empty();
     root_store.add_server_trust_anchors(
         webpki_roots::TLS_SERVER_ROOTS
@@ -20,43 +22,45 @@ fn main() {
                 )
             }),
     );
-    let config = rustls::ClientConfig::builder()
-        .with_safe_defaults()
-        .with_root_certificates(root_store)
-        .with_no_client_auth();
+    rustls::ClientConfig::builder()
+    .with_safe_defaults()
+    .with_root_certificates(root_store)
+    .with_no_client_auth()  
+}
 
+fn main() {
     let server_name = "api.binance.com".try_into().unwrap();
-    let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
+    let mut conn = rustls::ClientConnection::new(Arc::new(root_creator()), server_name).unwrap();
     let mut sock = TcpStream::connect("65.9.40.106:443").unwrap();
     let mut tls = rustls::Stream::new(&mut conn, &mut sock);
     let mut words = String::new();
-    while(true){
+    let (tx, rx) = mpsc::channel();
+    let body = concat!(
+        "GET /api/v3/time HTTP/1.1\r\n",
+        "Host: api.binance.com\r\n",
+        "Connection: closed\r\n",
+        "Accept-Encoding: identity\r\n",
+        "\r\n"
+    ).as_bytes();
+    loop{
         stdin().read_line(&mut words).expect("Input Error!");
         print!("{}", words.as_str());
         match words.as_str().trim(){  
             "b"=> {
-            break;
+             break;
         },
              _ => {
             words = String::new();
             }
         }
     }
-    let c= SystemTime::now().duration_since(UNIX_EPOCH).expect("get_current_unix_err").as_millis();
-    tls.write_all(
-        concat!(
-            "GET /api/v3/time HTTP/1.1\r\n",
-            "Host: api.binance.com\r\n",
-            "Connection: closed\r\n",
-            "Accept-Encoding: identity\r\n",
-            "\r\n"
-        )
-        .as_bytes(),
-    )
-    .unwrap();
-
+    thread::spawn(move || {
+       let c = SystemTime::now().duration_since(UNIX_EPOCH).expect("get_current_unix_err").as_millis();
+       tx.send(c).unwrap();
+    });
+    tls.write_all(body).expect("bad happened");
     let mut plaintext = Vec::new();
     tls.read_to_end(&mut plaintext).unwrap();
     stdout().write_all(&plaintext).unwrap();
-    print!("{:?}",c)
+    print!("{:?}",rx.recv().unwrap());
 }
